6aa47c3339823ee2e388ab60f40f5cef2fa4ff2f
Dan Anthony Gollayan <zdrgolayan@globe.com.ph>
2023-12-14 02:28:34 +0800

    fina;

--- lib/cdkdemo-stack.ts
+++ lib/cdkdemo-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './demoawspipeline-app-stack';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkdemoStack extends cdk.Stack {
@@ -22,5 +24,14 @@ export class CdkdemoStack extends cdk.Stack {
         ],
       }),
     })
+    const testingStage = democicdpipeline.addStage(new PipelineAppStage(this, 'test', {
+      env: { account: '971424090493', region: 'ap-southeast-2' }
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    const prodStage = democicdpipeline.addStage(new PipelineAppStage(this, 'prod', {
+      env: { account: '971424090493', region: 'ap-southeast-2' }
+    }));
   }
 }
